f191dd9e9ddcf3a44774f10aacf06550743927d0
regulad <regulad@regulad.xyz>
2022-12-15 16:39:24 -0500

    work on splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
+import NavDropdown from './components/NavDropdown/NavDropdown';
+import SplashText from "./components/SplashText/SplashText";
 
 function App() {
     return (
-        <div className="Placeholder">
-            <p className="Title">Regulad's Mouse House</p>
-            <p className="Subtitle">Coming soon to a webzone near you!</p>
-            <p>In the meantime, my website from 2021 can be found <a href={"https://regulad.xyz/web2021"} className={"link"}>here</a>.</p>
-        </div>
-    );
+        <>
+            <header className={"Main-header"}>
+                <div className={"Horizontal-box"}>
+                    <h1 className={"Title"}>Regulad's Mouse House</h1>
+                    <SplashText></SplashText>
+                </div>
+                <NavDropdown></NavDropdown>
+            </header>
+            <div className="Main-body">
+                <p className="Title">Regulad's Mouse House</p>
+                <p className="Subtitle">Coming soon to a webzone near you!</p>
+                <p>In the meantime, my website from 2021 can be found <a href={"https://regulad.xyz/web2021"} className={"link"}>here</a>.</p>
+            </div>
+        </>
+);
 }
 
 export default App;
---
+++ src/components/NavDropdown/NavDropdown.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./NavDropdown.css";
+
+export default class NavDropdown extends React.Component {
+    render() {
+        return (
+            <div className={"Nav-dropdown"}>
+
+            </div>
+        );
+    }
+}
