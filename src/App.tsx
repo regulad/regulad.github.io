d08e0f6aafecc1dc63951448d0ba9df2e890cac3
Parker Wahle <regulad@regulad.xyz>
2022-12-15 06:45:10 -0500

    Initial 2023 Commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="Placeholder">
+            <p className="Title">Regulad's Mouse House</p>
+            <p className="Subtitle">Coming soon to a webzone near you!</p>
+            <p>In the meantime, my website from 2021 can be found <a href={"https://regulad.xyz/web2021"} className={"link"}>here</a>.</p>
+        </div>
+    );
+}
+
+export default App;
